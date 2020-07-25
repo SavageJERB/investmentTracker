bda0c3a1e9e1e86eb83d685a53c6a813c7648e4b
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-07-24 23:48:07 -0700

    toggle working for search

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,16 @@
 //----------Sorting Watchlist: Each saved company
 $( () => $( "#sortable" ).sortable({axis: "y"}));
 
+
+//----------Toggle Search By
+$('.searchParams').hide();
+
+$('#params').click('click', () => {
+    $('.searchByTicker').hide();
+    $('.searchParams').show();
+})
+
+$('#tickerS').click('click', () => {
+    $('.searchByTicker').show();
+    $('.searchParams').hide();
+})
